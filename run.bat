3cf8ed623a863eba4dc0fdf0c8f96e3f90792f70
Carter Gordon <101619615+HiddenWaste@users.noreply.github.com>
2025-12-24 17:27:31 -0600

    Adding run.bat

---
+++ run.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Starting Dashboard System...
+
+:: Start the Flask Backend
+start "Flask Backend" cmd /k "python app.py"
+
+:: Move to react folder and start frontend
+cd dashboard-react-site
+start "React Frontend" cmd /k "npm start"
+
+echo Both servers are launching!
+pause
